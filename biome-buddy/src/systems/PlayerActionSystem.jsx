30762470983b30a216f0b14bfade1d07f36d4759
angie-nguyen <ann087@sdsc.edu>
2026-02-09 17:53:26 -0800

    apply growth rate multiplier based on player's choice

--- biome-buddy/src/systems/PlayerActionSystem.jsx
+++ biome-buddy/src/systems/PlayerActionSystem.jsx
@@ -1,10 +1,21 @@
+const GROWTH_RATE_MULTIPLIER = 2 
+
 class PlayerActionSystem extends System {
     constructor() {
         super("PlayerActionSystem")
+        this.speciesIDChoice = null // int - the species ID that the player chooses to take action on.
     }
 
     apply(context) {
-        // TODO: implement effects on population
+        if (this.speciesIDChoice == null) throw new Error("PlayerActionSystem requires a speciesIDChoice to be set before applying the system.")
+        const population = context.populations.get(this.speciesIDChoice)
+        if (population == null) throw new Error(`Population with species ID ${this.speciesIDChoice} not found in GameContext populations.`)
+        population.applyGrowthRateMultiplier(GROWTH_RATE_MULTIPLIER) 
+        this.speciesIDChoice = null // Reset the choice after applying the system
+    }
+
+    setSpeciesIDChoice(speciesID) {
+        this.speciesIDChoice = speciesID
     }
 }
 
