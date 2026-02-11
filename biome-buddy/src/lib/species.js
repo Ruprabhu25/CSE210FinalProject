534b86bc6f947074dff84062302326c524775bb1
Uliyaah Dionisio <86292164+uliyaah@users.noreply.github.com>
2026-02-10 18:36:42 -0800

    Revert species.js

--- biome-buddy/src/lib/species.js
+++ biome-buddy/src/lib/species.js
@@ -2,9 +2,6 @@
  * Species base class and consumer/producer subclasses.
  * Simple model for population, biomass and energy bookkeeping.
  */
-
-let _nextSpeciesId = 1
-
 export class Species {
   /**
    * @param {string} name
@@ -13,20 +10,37 @@ export class Species {
    * @param {number} population
    * @param {number} growthRate - fractional growth per update (e.g. 0.1 = +10%)
    */
-  constructor(name, energy, biomass) {
+  constructor(name, energy, biomass, population = 0, growthRate = 0) {
     this.name = String(name)
     this.energy = Number(energy) || 0
     this.biomass = Number(biomass) || 0
-    // assign a unique numeric id to each species instance
-    this.speciesid = _nextSpeciesId++
+    this.population = Number(population) || 0
+    this.growthRate = Number(growthRate) || 0
+  }
+
+  // directly update growth rate
+  setGrowthRate(newRate) {
+    this.growthRate = Number(newRate) || 0
   }
 
   getTotalBiomass() {
-    return this.biomass * this._population.getCurrentSize()
+    return this.biomass * this.population
   }
 
   getTotalEnergy() {
-    return this.energy * this._population.getCurrentSize()
+    return this.energy * this.population
+  }
+
+  /**
+   * Update population according to growthRate.
+   * Keeps population non-negative. Population may be fractional; caller
+   * can round if integer counts are required.
+   * @returns {number} new population
+   */
+  updatePopulation() {
+    const next = this.population + this.population * this.growthRate
+    this.population = Math.max(0, next)
+    return this.population
   }
 
   toJSON() {
@@ -34,9 +48,36 @@ export class Species {
       name: this.name,
       energy: this.energy,
       biomass: this.biomass,
-      population: this._population.getCurrentSize(),
-      growthRate: this._population.baseGrowthRate,
-      speciesid: this.speciesid,
+      population: this.population,
+      growthRate: this.growthRate,
     }
+  }
+}
+
+export class Producer extends Species {
+  constructor(name, energy, biomass, population = 0, growthRate = 0.1) {
+    super(name, energy, biomass, population, growthRate)
+    this.trophic = 'producer'
+  }
+}
+
+export class PrimaryConsumer extends Species {
+  constructor(name, energy, biomass, population = 0, growthRate = 0.05) {
+    super(name, energy, biomass, population, growthRate)
+    this.trophic = 'primary-consumer'
+  }
+}
+
+export class SecondaryConsumer extends Species {
+  constructor(name, energy, biomass, population = 0, growthRate = 0.03) {
+    super(name, energy, biomass, population, growthRate)
+    this.trophic = 'secondary-consumer'
+  }
+}
+
+export class TertiaryConsumer extends Species {
+  constructor(name, energy, biomass, population = 0, growthRate = 0.01) {
+    super(name, energy, biomass, population, growthRate)
+    this.trophic = 'tertiary-consumer'
   }
 }
