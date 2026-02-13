39bf5e2163c52cb37e676f718c47fd73d244d3fa
angie-nguyen <ann087@sdsc.edu>
2026-02-13 10:35:56 -0800

    use species name instead of id, allow for no action

--- biome-buddy/src/systems/PlayerActionSystem.jsx
+++ biome-buddy/src/systems/PlayerActionSystem.jsx
@@ -5,19 +5,20 @@ const GROWTH_RATE_MULTIPLIER = 2
 class PlayerActionSystem extends System {
     constructor() {
         super("PlayerActionSystem")
-        this.speciesIDChoice = null // int - the species ID that the player chooses to take action on.
+        this.chosenSpeciesName = "" // This will be set by the UI when the player makes a choice
     }
 
     apply(context) {
-        if (this.speciesIDChoice == null) throw new Error("PlayerActionSystem requires a speciesIDChoice to be set before applying the system.")
-        const population = context.populations.get(this.speciesIDChoice)
-        if (population == null) throw new Error(`Population with species ID ${this.speciesIDChoice} not found in GameContext populations.`)
-        population.applyGrowthRateMultiplier(GROWTH_RATE_MULTIPLIER) 
-        this.speciesIDChoice = null // Reset the choice after applying the system
+        if (this.chosenSpeciesName != "") {
+            const population = context.populations.get(this.chosenSpeciesName)
+            if (population == null) throw new Error(`Population with species name ${this.chosenSpeciesName} not found in GameContext populations.`)
+            population.applyGrowthRateMultiplier(GROWTH_RATE_MULTIPLIER) 
+        }
+        this.chosenSpeciesName = "" // Reset the choice after applying the system
     }
 
-    setSpeciesIDChoice(speciesID) {
-        this.speciesIDChoice = speciesID
+    setChosenSpeciesName(speciesName) {
+        this.chosenSpeciesName = speciesName
     }
 }
 
