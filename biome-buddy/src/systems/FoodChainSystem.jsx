401e40bdc725574e03490efa9dc20b0a971c65c1
shambhavi-20 <shmittal@ucsd.edu>
2026-02-13 17:35:01 -0800

    fixed bugs based on name chnage form speciesid to name

--- biome-buddy/src/systems/FoodChainSystem.jsx
+++ biome-buddy/src/systems/FoodChainSystem.jsx
@@ -1,15 +1,13 @@
 import System from "./System"
-import {calculateRatioDeviation, deviationToScore } from '../helperFunctions/healthCalculateHelperFunctions'
+const MULTIPLIER_DIVISION_FACTOR = 2;
 
-const MULTIPLIER_DIVISION_FACTOR = 4;
 class FoodChainSystem extends System {
     constructor() {
         super("FoodChainSystem")
     }
-
-    getTotalPopulation(speciesIds) {
+    getTotalPopulation(names) {
         let total = 0
-        for (const id of speciesIds) {
+        for (const id of names) {
             const pop = this.populations.get(id)
             if (pop) {
                 total += pop.getCurrentSize()
@@ -17,65 +15,61 @@ class FoodChainSystem extends System {
         }
         return total
     }
-
     apply(context) {
         this.trophicLevels = context.trophicLevels
         this.populations = context.populations
+        
 
         for (let i = 0; i < this.trophicLevels.length - 1; i++) {
             const preyLevel = this.trophicLevels[i]
             const predatorLevel = this.trophicLevels[i + 1]
-
-            const preySpeciesIds = Object.keys(preyLevel.speciesMap)
-            const predatorSpeciesIds = Object.keys(predatorLevel.speciesMap)
-
+            const preyNames = Object.keys(preyLevel.speciesMap)
+            const predatorNames = Object.keys(predatorLevel.speciesMap)
             // Total prey and predator available
-            const totalPrey = this.getTotalPopulation(preySpeciesIds)
-            const totalPredator = this.getTotalPopulation(predatorSpeciesIds)
-
+            const totalPrey = this.getTotalPopulation(preyNames)
+            const totalPredator = this.getTotalPopulation(predatorNames)
             if (totalPrey === 0 || totalPredator === 0) {
                 continue
             }; 
-            const actualRatio = totalPrey / totalPredator
-            const idealRatio = preyLevel.idealRatio / predatorLevel.idealRatio
-            const deviation = calculateRatioDeviation(totalPrey, totalPredator, preyLevel.idealRatio, predatorLevel.idealRatio,)
 
-            // Allow 20% tolerance
-            const multiplier = deviationToScore(deviation, 0.2)
-            if (multiplier === 1) {
-                continue
-            }; 
+            const actualRatio = totalPrey / totalPredator;
+            const idealRatio = preyLevel.idealRatio / predatorLevel.idealRatio;
+            const deviation = Math.abs(actualRatio - idealRatio) / idealRatio;
+            if (deviation < 0.2) {
+                continue;
+            }
 
             // If prey are scarce relative to predators, predators decline and prey decline slightly due to overconsumption
             if (actualRatio < idealRatio) {
-                for (const id of predatorSpeciesIds) {
-                    const pop = this.populations.get(id)
+                for (const id of predatorNames) {
+                    const pop = this.populations.get(id);
                     if (pop) {
-                        pop.updatePopulationByMortalityRate(1 + (1-multiplier)*MULTIPLIER_DIVISION_FACTOR)
+                        pop.updatePopulationByMortalityRate(1 + deviation * MULTIPLIER_DIVISION_FACTOR);
                     }
                 }
-                for (const id of preySpeciesIds) {
-                    const pop = this.populations.get(id)
+                for (const id of preyNames) {
+                    const pop = this.populations.get(id);
                     if (pop) {
-                        pop.updatePopulationByMortalityRate(1 + (1-multiplier)*MULTIPLIER_DIVISION_FACTOR)
+                        pop.updatePopulationByMortalityRate(1 + deviation * (MULTIPLIER_DIVISION_FACTOR));
                     }
                 }
             } 
             else {
                 // If prey are abundant, predators grow and prey decrease slightly
-                for (const id of predatorSpeciesIds) {
-                    const pop = this.populations.get(id)
+                for (const id of predatorNames) {
+                    const pop = this.populations.get(id);
                     if (pop) {
-                        pop.updatePopulationByGrowthRate(1 + deviation*MULTIPLIER_DIVISION_FACTOR)
+                        pop.updatePopulationByGrowthRate(deviation * MULTIPLIER_DIVISION_FACTOR);
                     }
                 }
-                for (const id of preySpeciesIds) {
-                    const pop = this.populations.get(id)
+                for (const id of preyNames) {
+                    const pop = this.populations.get(id);
                     if (pop) {
-                        pop.updatePopulationByMortalityRate(1 - Math.max(0, multiplier/MULTIPLIER_DIVISION_FACTOR))
+                        pop.updatePopulationByMortalityRate(1 - deviation);
                     }
                 }
             }
+            console.log(`populations after FoodChainSystem between ${preyLevel.name} and ${predatorLevel.name}: ${Array.from(this.populations.values()).map(p => p.getCurrentSize())}`)
         }
     }
 }
