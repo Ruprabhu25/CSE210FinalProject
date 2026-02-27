50d3b25bbccd8f5c2bd25ff163241a08d1c54ee1
Uliyaah Dionisio <86292164+uliyaah@users.noreply.github.com>
2026-02-26 16:59:50 -0800

    Species sprites for fox, hawk, grass, and bunny added?

---
+++ biome-buddy/src/game/speciesSprites.jsx
@@ -0,0 +1,11 @@
+import foxSprite from '../assets/species/fox.png'
+import grassSprite from '../assets/species/grass.png'
+import hawkSprite from '../assets/species/hawk.png'
+import rabbitSprite from '../assets/species/rabbit.png'
+
+export const speciesSprites = {
+  Grass: grassSprite,
+  Rabbit: rabbitSprite,
+  Fox: foxSprite,
+  Hawk: hawkSprite,
+}
