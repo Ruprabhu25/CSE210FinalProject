3feb04062456f3fa834b71002f941d7cc4fb8466
angie-nguyen <ann087@sdsc.edu>
2026-03-05 21:14:27 -0800

    remove parallel tests for CI :(

--- biome-buddy/playwright.config.ts
+++ biome-buddy/playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Run parallel tests on CI. */
-  workers: process.env.CI ? 5 : undefined,
+  /* No parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
